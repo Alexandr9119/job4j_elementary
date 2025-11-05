13697971bf399983339bd2d0c7836c518ed53f51
NevolinAN <xattab9109@gmail.com>
2025-11-05 14:28:33 +0500

    Арифметические операции. [#185 #66414]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -5,6 +5,7 @@ public class Calculator {
     public static void main(String[] args) {
         int one = 1;
         int two = 2;
+        int three = 3;
         int four = 4;
         int five = 5;
         int six = 6;
